2c0567d52b22aecff5b59855bc84e9682e6ae009
user <user@Linux.myguest.virtualbox.org>
2023-04-03 18:40:20 +0300

    Try3

--- a1/a1.c
+++ a1/a1.c
@@ -73,8 +73,8 @@ int list_normal(char *path, char *name_starts_with, int has_perm_execute)
             }
         }
     }
-    return 1;
     closedir(dir);
+    return 1;
 }
 
 int list_recursiv(char *path, char *name_starts_with, int has_perm_execute)
@@ -138,8 +138,8 @@ int list_recursiv(char *path, char *name_starts_with, int has_perm_execute)
             }
         }
     }
-    return 1;
     closedir(dir);
+    return 1;
 }
 
 int parse_SF_file(char *path)
@@ -154,8 +154,10 @@ int parse_SF_file(char *path)
     if (read(fd, &magic, 1) != 1 || magic != 'd')
     {
         printf("ERROR\nwrong magic\n");
+        // printf("ERROR\nwrong magic\n%c %d\n",magic,magic);
         return -1;
     }
+    // printf("%c====%d\n",magic,magic);
 
     lseek(fd, -3, SEEK_END);
     read(fd, &header_size, 2);
@@ -186,6 +188,7 @@ int parse_SF_file(char *path)
 
         read(fd, &(sections[i].sect_name), 17);
         sections[i].sect_name[17] = '\0';
+
         read(fd, &(sections[i].sect_type), 2);
         if (sections[i].sect_type != 52 && sections[i].sect_type != 41 && sections[i].sect_type != 89)
         {
@@ -204,7 +207,7 @@ int parse_SF_file(char *path)
     {
         printf("section%d: %s %d %d\n", i + 1, sections[i].sect_name, sections[i].sect_type, sections[i].sect_size);
     }
-
+    close(fd);
     return 1;
 }
 
@@ -242,24 +245,24 @@ int extract_from_SF(char *path, int section, int line)
         return -1;
     }
 
-    Section_Header sections[no_of_sections + 1];
-    for (int i = 0; i < no_of_sections; i++)
+    Section_Header sections[no_of_sections+1];
+    for (int i = 1; i <= no_of_sections; i++)
     {
-        strcpy(sections[i + 1].sect_name, "");
-        sections[i + 1].sect_type = 0;
-        sections[i + 1].sect_offset = 0;
-        sections[i + 1].sect_size = 0;
+        strcpy(sections[i].sect_name, "");
+        sections[i].sect_type = 0;
+        sections[i].sect_offset = 0;
+        sections[i].sect_size = 0;
 
-        read(fd, &(sections[i + 1].sect_name), 17);
-        sections[i + 1].sect_name[17] = '\0';
-        read(fd, &(sections[i + 1].sect_type), 2);
-        if (sections[i + 1].sect_type != 52 && sections[i + 1].sect_type != 41 && sections[i + 1].sect_type != 89)
+        read(fd, &(sections[i].sect_name), 17);
+        sections[i].sect_name[17] = '\0';
+        read(fd, &(sections[i].sect_type), 2);
+        if (sections[i].sect_type != 52 && sections[i].sect_type != 41 && sections[i].sect_type != 89)
         {
             printf("ERROR\ninvalid section\n");
             return -1;
         }
-        read(fd, &(sections[i + 1].sect_offset), 4);
-        read(fd, &(sections[i + 1].sect_size), 4);
+        read(fd, &(sections[i].sect_offset), 4);
+        read(fd, &(sections[i].sect_size), 4);
     }
 
     lseek(fd, sections[section].sect_offset, SEEK_SET);
@@ -281,11 +284,34 @@ int extract_from_SF(char *path, int section, int line)
         }
     }
 
+    // char *section_content, *pos;
+
+    // nrLines = 1;
+    // lseek(fd, sections[section-1].sect_offset, SEEK_SET);
+    // section_content = (char *)malloc(sizeof(char) * sections[section-1].sect_size);
+    // read(fd, section_content, sections[section-1].sect_size);
+    // pos = section_content;
+    // do
+    // {
+    //     pos = strchr(pos, '\x0A');
+
+    //     if (pos != NULL)
+    //     {
+    //         pos++;
+    //         // printf("%s\n",c);
+    //         nrLines++;
+    //     }
+
+    // } while (pos != NULL);
+    // printf("Numar de linii %d\n",nrLines);
+    // printf("%s\n",section_content);
+    // free(section_content);
+
     lseek(fd, sections[section].sect_offset, SEEK_SET);
 
     int startPrinting = 0;
-    for (int i = 0; i < sections[section].sect_size; i++)
-    {
+     for (int i = 0; i < sections[section].sect_size; i++)
+     {
 
         if (read(fd, &c, 1) != 1)
         {
@@ -307,9 +333,46 @@ int extract_from_SF(char *path, int section, int line)
                 startPrinting = 0;
         }
     }
+
+//     lseek(fd, sections[section-1].sect_offset, SEEK_SET);
+//     section_content = (char *)realloc(section_content,sizeof(char) * sections[section-1].sect_size);
+//     read(fd, section_content, sections[section-1].sect_size);
+//     lseek(fd, -sections[section-1].sect_size, SEEK_CUR);
+//     pos = section_content;
+//     do{
+//         pos = strchr(pos, '\x0A');
+
+//         if (pos != NULL)
+//         {
+//             pos++;
+//             nrLines--;
+            
+//         }
+//         if (nrLines == line)
+//         {
+//             printf("Numar de linii %d\n",nrLines);
+//             printf("SUCCESS\n");
+//             c = 0;
+//             //int i=0;
+//             lseek(fd, pos-section_content, SEEK_CUR);
+//             while (c != '\x0A')
+//             //while(i <= sections[section-1].sect_size)
+//             {
+//                 if (read(fd, &c, 1) != 1)
+//                 {
+//                     perror("Eroare la citire");
+//                     return -1;
+//                 }
+//                 printf("%c", c);
+//                 //i++;
+//             }
+//             return 1;
+//        }
+//    } while (pos != NULL);
+
+    close(fd);
     return -1;
 }
-
 int SF(char *path)
 {
     off_t fd;
@@ -321,10 +384,13 @@ int SF(char *path)
 
     if (read(fd, &magic, 1) != 1 || magic != 'd')
     {
-        //printf("ERROR\ninvalid file\n");
+        // printf("SSSSSGANGAEBGUABGUBAHFBAHUBFUHIABFBUHABFUABFB\n%s\n\n\n\n",path);
         return -1;
     }
-
+    // printf("%c--%d\n\n",magic,magic);
+    // printf("SIUU/%s\n",path);
+    // if(strcmp(path,"./test_root/VsJ7I7/3wRUUnYDx4/qpaWgg0SRp/vsIo3cmD/qQBAm2HxR/lLuDR0Jrn.pEo")==0)
+    // parse_SF_file(path);
     lseek(fd, -3, SEEK_END);
     read(fd, &header_size, 2);
 
@@ -333,54 +399,85 @@ int SF(char *path)
     read(fd, &version, 2);
     if (version < 41 || version > 151)
     {
-        //printf("ERROR\ninvalid file\n");
         return -1;
     }
 
     read(fd, &no_of_sections, 1);
     if (no_of_sections < 5 || no_of_sections > 19)
     {
-        //printf("ERROR\nwinvalid file\n");
         return -1;
     }
 
-    Section_Header sections[no_of_sections + 1];
+    Section_Header sections[no_of_sections];
     for (int i = 0; i < no_of_sections; i++)
     {
-        strcpy(sections[i + 1].sect_name, "");
-        sections[i + 1].sect_type = 0;
-        sections[i + 1].sect_offset = 0;
-        sections[i + 1].sect_size = 0;
+        strcpy(sections[i].sect_name, "");
+        sections[i].sect_type = 0;
+        sections[i].sect_offset = 0;
+        sections[i].sect_size = 0;
 
-        read(fd, &(sections[i + 1].sect_name), 17);
-        sections[i + 1].sect_name[17] = '\0';
-        read(fd, &(sections[i + 1].sect_type), 2);
-        if (sections[i + 1].sect_type != 52 && sections[i + 1].sect_type != 41 && sections[i + 1].sect_type != 89)
+        read(fd, &(sections[i].sect_name), 17);
+        sections[i].sect_name[17] = '\0';
+
+        read(fd, &(sections[i].sect_type), 2);
+        if (sections[i].sect_type != 52 && sections[i].sect_type != 41 && sections[i].sect_type != 89)
         {
-            //printf("ERROR\ninvalid section\n");
             return -1;
         }
-        read(fd, &(sections[i + 1].sect_offset), 4);
-        read(fd, &(sections[i + 1].sect_size), 4);
+        read(fd, &(sections[i].sect_offset), 4);
+        read(fd, &(sections[i].sect_size), 4);
     }
 
-    int nrLines = 1;
-    char c = 0;
+    int nrLines;
+    // char c = 0;
+    // for (int section = 0; section < no_of_sections; section++)
+    // {
+    //     nrLines = 1;
+    //     lseek(fd, sections[section].sect_offset, SEEK_SET);
+    //     for (int i = 0; i < sections[section].sect_size; i++)
+    //     {
+
+    //         read(fd, &c, 1);
+    //         if (c == '\x0A')
+    //         {
+    //             nrLines++;
+    //         }
+    //         if (nrLines > 16)
+    //         {
+    //             return 1;
+    //         }
+    //     }
+    // }
+
+    char *section_content, *c;
     for (int section = 0; section < no_of_sections; section++)
     {
+        nrLines = 1;
         lseek(fd, sections[section].sect_offset, SEEK_SET);
-        for (int i = 0; i < sections[section].sect_size; i++)
+        section_content = (char *)malloc(sizeof(char) * sections[section].sect_size);
+        read(fd, section_content, sections[section].sect_size);
+        c = section_content;
+        do
         {
+            c = strchr(c, '\x0A');
 
-            read(fd, &c, 1);
-            if (c == '\x0A')
+            if (c != NULL)
             {
+                c++;
+                // printf("%s\n",c);
                 nrLines++;
             }
-        }
-        if(nrLines>=16)
-            return 1;
+            if (nrLines > 16)
+            {
+                free(section_content);
+                return 1;
+            }
+
+        } while (c != NULL);
+        // printf("%s\n",section_content);
+        // free(section_content);
     }
+    close(fd);
     return -1;
 }
 
@@ -388,10 +485,10 @@ int findall_SF(char *path)
 {
     DIR *dir = NULL;
     struct dirent *entry = NULL;
+    dir = opendir(path);
     char fullPath[512];
     struct stat statbuf;
 
-    dir = opendir(path);
     if (dir == NULL)
     {
         perror("ERROR\ninvalid directory path\n");
@@ -407,20 +504,33 @@ int findall_SF(char *path)
         if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
         {
             snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
-            if (lstat(fullPath, &statbuf) == 0)
-            {
-                if(SF(fullPath));
-                printf("%s\n", fullPath + 2);
+            // if (lstat(fullPath, &statbuf) == 0)
+            // {
 
-                if (S_ISDIR(statbuf.st_mode))
-                {
-                    findall_SF(fullPath);
-                }
+            //     if (S_ISDIR(statbuf.st_mode))
+            //     {
+            //         findall_SF(fullPath);
+            //     }
+            //     else
+            //     {
+            //         if (SF(fullPath) == 1)
+            //             printf("%s\n", fullPath + 2);
+            //     }
+
+            // }
+
+            lstat(fullPath, &statbuf);
+            if (!S_ISDIR(statbuf.st_mode))
+                if (SF(fullPath) == 1)
+                    printf("%s\n", fullPath + 2);
+            if (S_ISDIR(statbuf.st_mode))
+            {
+                findall_SF(fullPath);
             }
         }
     }
-    return 1;
     closedir(dir);
+    return 1;
 }
 
 int main(int argc, char **argv)
@@ -492,8 +602,7 @@ int main(int argc, char **argv)
                 {
                     findall = 1;
                 }
-                // free(temp_argv);
-                // free(name_filtration);
+                free(temp_argv);
             }
         }
     }
@@ -523,10 +632,21 @@ int main(int argc, char **argv)
     {
         extract_from_SF(path, section, line);
     }
-    if(findall == 1)
+    if (findall == 1)
     {
         findall_SF(path);
     }
+
+    // char string[]={"/home/test/sample"};
+    // char* pos=strchr(string,'/');
+
+    // while(pos!=NULL)
+    // {
+    //     pos = pos+1;
+    //     pos=strchr(pos,'/');
+
+    //     printf("SIuuuu\n");
+    // }
 
     free(path);
     free(name_starts_with);
